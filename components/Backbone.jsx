8d5d9c9f2c45abb3a27a060e9d17af6d5240e5f7
eliashenriksen <79273139+eliashenriksen@users.noreply.github.com>
2023-02-24 03:27:54 +0100

    core logic work
    Added the main support for finding synonyms that have a matching emoji, if the word that was used returned 0 emoji results.

--- components/Backbone.jsx
+++ components/Backbone.jsx
@@ -1,7 +1,9 @@
 import data from '@emoji-mart/data';
 import { init, SearchIndex } from 'emoji-mart';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import _ from 'lodash';
+import axios from "axios";
+import { DATAMUSE_BASE_URL } from "@/constants/api";
 
 export default function Backbone() {
 
@@ -17,7 +19,8 @@ export default function Backbone() {
         return emoji.skins[0].native
       })
 
-      //if results length 0 fetch word api similar words and test those, select first match
+      //If results length is greater than 0 that means atleast 1 emoji was found for the input word (if more than 1 emoji was found, we randomly pick 1 with the help of lodash sample).
+      //If results length is 0 then no emojis were found and we call the similar word API to try again with a different word. Repeat untill we can find an emoji.
       if (results.length > 0) {
         const randomSingleEmojiFromArray = _.sample(results);
         console.log(randomSingleEmojiFromArray);
@@ -25,8 +28,45 @@ export default function Backbone() {
 
         //https://stackoverflow.com/questions/67309672/how-to-prevent-react-state-overwrites-due-to-asynchronous-code-and-websocket-rac
         setStoryOutputEmojis(prevState => [...prevState, randomSingleEmojiFromArray]);
+      } else {
+        async function findSimilarWord() {
+          try {
+            const response = await axios.get(DATAMUSE_BASE_URL + `/words?ml=${value}`);
+            const data = response.data;
+
+            async function deepSearch(value) {
+              const emojis = await SearchIndex.search(value);
+              const results = emojis.map((emoji) => {
+                return emoji.skins[0].native
+              })
+              if (results.length > 0) {
+                // console.log(results);
+                const randomSingleEmojiFromArray = _.sample(results);
+                console.log(randomSingleEmojiFromArray);
+                search(randomSingleEmojiFromArray);
+                return randomSingleEmojiFromArray;
+              } else {
+                // console.log("no matches");
+                return "";
+              }
+            }
+
+            for (let i = 0; i < data.length; i++) {
+              console.log(data[i].word);
+              const deepSearchResults = await deepSearch(data[i].word);
+              if (deepSearchResults) {
+                break;
+              }
+            }
+
+          } catch (error) {
+            console.log(error);
+          }
+        }
+        findSimilarWord();
       }
     }
+    //Add override for prepositions here (seperate override file import?).
     search(searchValue);
   }
 
